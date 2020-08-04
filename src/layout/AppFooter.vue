6e2852bc2544965a37b39213efbb4f3c89f4e3b2
Jonas <mail@jonasgoebel.net>
2020-08-04 23:26:07 +0200

    added AppHeader and Footer

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>&copy; 2020 <a href="https://jonasgoebel.net">Jonas Göbel</a></p>
+</template>
+<script>
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+};
+</script>
+<style>
+</style>
