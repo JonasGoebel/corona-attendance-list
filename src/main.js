b86ba4c17b8425c6437ef447a1ddbb76af5fa931
Jonas <mail@jonasgoebel.net>
2020-08-17 22:40:06 +0200

    added i18n-vue translation plugin

--- src/main.js
+++ src/main.js
@@ -16,13 +16,16 @@
 
 */
 import Vue from "vue";
+import VueI18n from 'vue-i18n'
 import * as Sentry from '@sentry/browser';
 import { Vue as VueIntegration } from '@sentry/integrations';
 import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
-import './registerServiceWorker'
+import './registerServiceWorker';
+import i18n from './i18n'
 
+// load sentry error reporting service
 Sentry.init({
   dsn: process.env.VUE_APP_SENTRY_URL,
   integrations: [new VueIntegration({Vue, attachProps: true})],
@@ -32,5 +35,6 @@ Vue.config.productionTip = false;
 Vue.use(Argon);
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
